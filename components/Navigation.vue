cc488226bcf48db05081efae6143d7e5d5d54b08
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2020-03-27 18:41:09 +0300

    make navigation a separate component

---
+++ components/Navigation.vue
@@ -0,0 +1,79 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="primary" fixed="top" sticky>
+    <b-navbar-toggle target="nav-collapse" />
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item to="/">Home</b-nav-item>
+      </b-navbar-nav>
+
+      <b-navbar-nav v-if="loggedIn">
+        <b-nav-item to="/apps">Apps</b-nav-item>
+        <b-nav-item to="/suites">Suites</b-nav-item>
+        <b-nav-item to="/create-test">Create test</b-nav-item>
+      </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto" v-if="loggedIn">
+        <b-button class="mx-1" variant="success" to="/profile">
+          Profile
+        </b-button>
+        <b-button class="mx-1" variant="success" @click="logout"
+          >Logout</b-button
+        >
+      </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto" v-if="!loggedIn">
+        <b-nav-form>
+          <b-form-input
+            class="mx-1"
+            placeholder="email"
+            v-model="email"
+            type="email"
+          />
+          <b-form-input
+            class="mx-1"
+            placeholder="password"
+            v-model="password"
+            type="password"
+          />
+          <b-button
+            class="mx-1"
+            @click.prevent="login"
+            variant="success"
+            type="submit"
+          >
+            Login
+          </b-button>
+        </b-nav-form>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import axios from '~/plugins/axios'
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  computed: {
+    ...mapGetters({ loggedIn: 'auth/loggedIn' })
+  },
+  methods: {
+    login() {
+      this.$store.dispatch('auth/login', {
+        email: this.email,
+        password: this.password
+      })
+      this.email = ''
+      this.password = ''
+    },
+    ...mapActions({ logout: 'auth/logout' })
+  }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,19 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="primary" fixed="top" sticky>
-      <b-navbar-toggle target="nav-collapse" />
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item to="/">Home</b-nav-item>
-          <b-nav-item to="/create-test">Tests</b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+    <Navigation />
     <nuxt />
   </div>
 </template>
+
+<script>
+import Navigation from '~/components/Navigation'
+
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
 
 <style>
 html {
@@ -32,34 +33,5 @@ html {
 *:after {
   box-sizing: border-box;
   margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
