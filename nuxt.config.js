a860876b8a13a61fbbb58f391f86fa295d6895f0
Omer Tarik Koc <omertarikkoc@gmail.com>
2020-04-02 12:49:22 -0500

    vue notificaitons added

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,8 +40,21 @@ export default {
    */
   modules: [
     // Doc: https://bootstrap-vue.js.org
-    'bootstrap-vue/nuxt'
+    'bootstrap-vue/nuxt',
+    '@nuxtjs/toast',
   ],
+  toast: {
+    position: 'top-center',
+    register: [ // Register custom toasts
+      {
+        name: 'my-error',
+        message: 'Oops...Something went wrong',
+        options: {
+          type: 'error'
+        }
+      }
+    ]
+  },
   /*
    ** Build configuration
    */
--- store/auth.js
+++ store/auth.js
@@ -19,16 +19,25 @@ export const mutations = {
   }
 }
 
+// try {
+//   this.$toast.show('Logging in...')
+//   await this.$axios.$post('auth/login')
+//   this.$toast.success('Successfully authenticated')
+// } catch(e){
+//   this.$toast.global.my_error() //Using custom toast
+//   this.$toast.error('Error while authenticating')
+// }
+
 export const actions = {
   login({ commit }, { email, password }) {
     axios
       .post('/auth/login', { email, password })
       .then(({ data }) => {
-        // TODO: add vue-notification here
+        this.$toast.success('Successfully authenticated', {duration: 2000})
         commit('login', { user: data.user })
       })
       .catch(err => {
-        // TODO: add vue-notification here
+        this.$toast.error('Error while authenticating', {duration: 2000})
         console.error(err)
       })
   },
@@ -37,11 +46,11 @@ export const actions = {
       .get('/auth/logout')
       .then(_response => {
         commit('logout')
-        // TODO: add vue-notification here
+        this.$toast.success('Successfully logged out.', {duration: 2000})
         console.info('Successfully logged out.')
       })
       .catch(err => {
-        // TODO: add vue-notification here
+        this.$toast.error('Error while logging out', {duration: 2000})
         console.error(err)
       })
   }
