26c4599e66455f9d8810c708853c2bcdb2a207d2
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2020-04-19 16:47:11 +0300

    Add /cases page

--- pages/cases/index.vue
+++ pages/cases/index.vue
@@ -1,13 +1,38 @@
 <template>
-  <b-container>
+  <b-container class="container-fluid">
     <b-row>
-      <b-col />
-      <b-col cols="6">
+      <b-col>
         <b-button class="mb-2" to="/cases/new" variant="primary">
           Create new case
         </b-button>
+
+        <b-table hover sticky-header stripped :items="cases" />
       </b-col>
-      <b-col />
     </b-row>
   </b-container>
 </template>
+
+<script>
+import axios from '~/plugins/axios'
+
+export default {
+  middleware: 'authenticated',
+  head() {
+    return { title: 'Cases' }
+  },
+  asyncData() {
+    return axios
+      .get('/cases/list')
+      .then(({ data }) => {
+        return { cases: data.cases }
+      })
+      .catch(error => {
+        console.error(error)
+        return {}
+      })
+  },
+  data() {
+    return {}
+  }
+}
+</script>
