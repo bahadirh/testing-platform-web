d5ae9add1af9d1cb0d281d8e78f0e5f2b857e4bd
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2020-04-19 12:32:46 +0300

    Update authentication middleware

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,6 +1,5 @@
 export default ({ store, redirect }) => {
   if (!store.getters['auth/loggedIn']) {
-    // TODO: fix it: make it run AFTER localStorage is loaded, somehow
-    // redirect('/signup')
+    redirect('/signup')
   }
 }
